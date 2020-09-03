2d0debff00a0c24393488478e59df2f0c102d8d2
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2020-09-03 18:55:30 -0400

    Fixed date bug

--- utils/generate_csv.py
+++ utils/generate_csv.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     anglicism_pd = pd.read_csv(ANGLICISM_INDEX, error_bad_lines=False, parse_dates=['date'])
-    anglicism_pd['date'] = pd.to_datetime(anglicism_pd.date, utc=True)
+    anglicism_pd['date'] = pd.to_datetime(anglicism_pd.date, errors='coerce', utc=True)
 
     mydf = anglicism_pd.query("date.dt.month==@args.month and date.dt.year==@args.year")
     mydf.to_csv(DATA_FOLDER + TO_MONTH_NAME[args.month]+str(args.year)+".csv", index=False)
