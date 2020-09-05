4448d3f10e18eccbd33752b8ffaab1715d061eaa
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2020-09-05 19:48:56 -0400

    Changes to viz

--- scripts/vizualize.py
+++ scripts/vizualize.py
@@ -6,6 +6,7 @@ import plotly
 import plotly.graph_objs as go
 from datetime import datetime, timedelta
 from collections import Counter
+
 import sys
 sys.path.append("/home/ealvarezmellado/lazaro/utils/")
 from constants import ANGLICISM_INDEX, ARTICLES_INDEX, TO_BE_TWEETED_PATTERN, PATH_TO_VIZ
@@ -51,13 +52,14 @@ def get_table_ultimos_angl(my_title):
     df_from_each_file = (pd.read_csv(f) for f in paths)
     mydf = pd.concat(df_from_each_file, ignore_index=True)
     #print(mydf.to_string())
-    mydf['date'] = pd.to_datetime(mydf.date, utc=True)
+    mydf['date'] = pd.to_datetime(mydf.date, errors='coerce', utc=True)
     mydf["date"] = mydf["date"].dt.strftime('%d/%m/%Y')
     mydf["newspaper"] = mydf["newspaper"].map(MEDIOS)
     mydf["link"] = "<a href=\"" + mydf.url + "\">"+ mydf.newspaper + "</a>"
     mydf["context"] = "<i>" + mydf["context"] + "</i>"
     fig = go.Figure(data=[go.Table(
-        columnwidth=[100, 400, 100, 70],
+        #columnwidth=[100, 400, 100, 70],
+        #columnwidth=[20, 50, 20, 10],
         header=dict(values=["Anglicismo", "Contexto", "Medio", "Fecha"],
                     fill_color='slategray',
                     align='left', font=dict(family="Arial", size=12, color='white'),),
@@ -65,20 +67,23 @@ def get_table_ultimos_angl(my_title):
                    align='left'))
     ])
     #arranged_title = TITLES[my_title][0] + "<br>" + TITLES[my_title][1]
-    fig.update_layout(title_text=TITLES[my_title],
-                      font=dict(family="-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial"))
+
+    fig.update_layout(#title_text=TITLES[my_title],
+                      font=dict(family="-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial"),
+                      autosize=True)
+
     with open(PATH_TO_VIZ + my_title+'.html', 'w') as f:
         f.write(fig.to_html(include_plotlyjs='cdn'))
 
 
 
 def get_last_fortnight(df):
-    df['date'] = pd.to_datetime(df.date, utc=True)
+    df['date'] = pd.to_datetime(df.date, errors='coerce', utc=True)
     last_week = df.query("(@TODAY - date).days <= 15")
     return last_week
 
 def get_prev_fortnight(df):
-    df['date'] = pd.to_datetime(df.date, utc=True)
+    df['date'] = pd.to_datetime(df.date, errors='coerce', utc=True)
     last_week = df.query("@TODAY - date.dt.tz_convert(None)).dt.days => 15 and @TODAY - df['date'].dt.tz_convert(None)).dt.days <= 30")
     return last_week
 
@@ -92,27 +97,28 @@ def anglicisms_per_section(section):
 def build_graph(dataframe, list_of_words, since_week, my_title):
     df = dataframe.query("week>@since_week and borrowing==@list_of_words")
     df = df.loc[:, ['borrowing', 'my_week', 'freq']]
-    df['my_week'] = pd.to_datetime(df.my_week, utc=True)
+    df['my_week'] = pd.to_datetime(df.my_week, errors='coerce', utc=True)
     df.sort_values(by=['my_week', "freq"], inplace=True, ascending=False)
     fig = px.line(df,
                   x="my_week",
                   y="freq",
                   color='borrowing',
                   line_shape="spline",
-                  render_mode="svg",
+                  #render_mode="svg",
                   #log_y=True,
-                  labels={"my_week": "Tiempo", "freq": "Frecuencia <br>(por cada 100.000 palabras)"},
+                  labels={"my_week": "Tiempo", "freq": "Frecuencia"},
                   template="simple_white")
     fig.update_traces(mode="markers+lines")
-    fig.update_layout(legend_title_text='Palabra')
+    fig.update_layout(legend_title_text='Palabra', autosize=True)
 
 
     #layout = dict(updatemenus=updatemenus, title='Linear scale')
     fig2 = go.Figure(fig)
-    fig2.update_layout(title_text=TITLES[my_title],
-                      font=dict(family="-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial"))
+    fig2.update_layout(#title_text=TITLES[my_title],
+                       font=dict(family="-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial"))
 
-    fig2.update_layout(updatemenus=[
+    fig2.update_layout(
+        updatemenus=[
                           dict(
                               buttons=list([
                                   dict(label="Linear",
@@ -125,8 +131,13 @@ def build_graph(dataframe, list_of_words, since_week, my_title):
                           )])
     # fig = px.scatter(df, x="my_week", y="freq", color='borrowing', mode='lines+markers')
     #fig2.show()
+    """
+    print(plotly.offline.plot(fig2, include_plotlyjs=False, output_type='div'))
+    print("#############")
+    """
     with open(PATH_TO_VIZ + my_title + '.html', 'w') as f:
         f.write(fig2.to_html(include_plotlyjs='cdn'))
+
 
 
 """
@@ -136,7 +147,7 @@ for section in SECTIONS:
 get_table_ultimos_angl("latest")
 
 anglicism_pd = pd.read_csv(ANGLICISM_INDEX, error_bad_lines=False, parse_dates=['date'])
-anglicism_pd['date'] = pd.to_datetime(anglicism_pd.date, utc=True)
+anglicism_pd['date'] = pd.to_datetime(anglicism_pd.date, errors='coerce', utc=True)
 anglicism_pd['week'] = anglicism_pd["date"].dt.week
 #print(anglicism_pd)
 anglicism_pd['borrowing'] = anglicism_pd['borrowing'].replace(['selfies'], 'selfie')
@@ -145,7 +156,7 @@ anglicism_pd['borrowing'] = anglicism_pd['borrowing'].apply(
     lambda x: singularize(x) if not x.endswith(" data") else x)
 
 articles_pd = pd.read_csv(ARTICLES_INDEX, error_bad_lines=False, parse_dates=['date'])
-articles_pd['date'] = pd.to_datetime(articles_pd.date, utc=True)
+articles_pd['date'] = pd.to_datetime(articles_pd.date, errors='coerce', utc=True)
 articles_pd['week'] = articles_pd["date"].dt.week
 articles_pd['year'] = articles_pd["date"].dt.year
 anglicisms_per_week = anglicism_pd.groupby(by=['borrowing', 'week']).size().reset_index(name="Appearances").sort_values('Appearances', ascending=False)
@@ -159,7 +170,7 @@ words_per_week_dict = (words_per_week.T).to_dict()
 merged = pd.merge(anglicisms_per_week, words_per_week, on='week')
 #print(merged)
 merged["freq"] =  100000*(merged["Appearances"]/merged["tokens"])
-merged["my_week"] = pd.to_datetime(2020, format='%Y') + \
+merged["my_week"] = pd.to_datetime(2020, errors='coerce', format='%Y') + \
              pd.to_timedelta((merged.week.mul(7) - 3).astype(str)+ ' days')
 my_toptweenty = merged.query("week==@TODAY.week").sort_values(by=["freq"], ascending=False)["borrowing"].tolist()[:20]
 #print(merged)
