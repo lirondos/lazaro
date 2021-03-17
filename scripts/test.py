299960227d62589895fede38168c29ad227ab513
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2021-03-17 18:21:47 -0400

    scripts update

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,3 @@
-
-
 from typing import Mapping, Sequence, Dict, Optional, List, NamedTuple, Tuple, Counter, Iterable
 #import pymagnitude
 import csv
@@ -29,17 +27,15 @@ from datetime import datetime, timezone
 import time
 from spacy.lang.tokenizer_exceptions import URL_PATTERN
 import re
-import pandas as pd
 import sys
-sys.path.append("/home/ealvarezmellado/lazaro/utils/")
+sys.path.append("C:\\Users\\Elena\\Desktop\\lazaro\\utils")
+#sys.path.append("/home/ealvarezmellado/lazaro/utils/")
 from utils2 import BiasFeature, TokenFeature, UppercaseFeature, TitlecaseFeature, TrigramFeature, QuotationFeature, WordEnding, POStagFeature, WordVectorFeature, WordShapeFeature, WordVectorFeatureSpacy, BigramFeature, IsInDict, GraphotacticFeature, LemmaFeature, DigitFeature, PunctuationFeature, WordVectorFeatureNerpy, WordProbability, WordVectorFeatureNorm, SentencePositionFeature, BrownClusterFeature, HigherEnglishProbability, QuatrigramFeature, AllCapsFeature, PerplexityFeature, URLFeature, EmailFeature, TwitterFeature
 from utils2 import WindowedTokenFeatureExtractor, CRFsuiteEntityRecognizer, BILOUEncoder, BIOEncoder, IOEncoder, ScoringCounts, ScoringEntity, BMESEncoder, BIOESEncoder
 from constants import ANGLICISM_INDEX, TO_BE_TWEETED_PATTERN, AUTOMATICALLY_ANNOTATED_FOLDER, TO_BE_PREDICTED_FOLDER, CORPUS
 from utils import PUNC_REPEAT_RE, DIGIT_RE, UPPERCASE_RE, LOWERCASE_RE
 from utils import PRF1
-from secret import MY_HOST, MY_USERNAME, MY_PASS, MY_DB
-import mysql.connector
-from pattern.en import singularize
+
 
 
 
@@ -51,22 +47,18 @@ TODAY = datetime.now(timezone.utc).strftime('%d%m%Y')
 parser = argparse.ArgumentParser()
 
 
-parser.add_argument('--train_folder', type=str, help='Path to file with training data', default=CORPUS)
-parser.add_argument('--val_folder', type=str, help='Path to file with validation data', default=None)
-parser.add_argument('--cross_validation', action='store_true', help='Path to file with validation data', default=False)
-parser.add_argument('--max_iterations', type=int, default=None, help='Max interations (default None)')
-parser.add_argument('--c1',type=float, default=0.05, help='L1 regularization coefficient (default 0.01)')
-parser.add_argument('--c2',type=float, default=0.01, help='L2 regularization coefficient (default 0.01)')
-parser.add_argument('--delta',type=float, default=1e-3, help='delta')
+parser.add_argument('--test', type=str, help='Path to list file listing training files')
+parser.add_argument('--predicted', type=str, help='Path where predicted docs will be stored')
+parser.add_argument('--model', type=str, help='Path where CRF model is stored')
 parser.add_argument('--encoder', type=str, default='BIO', help = 'Encoding to be apply (BIO, IO, BILOU; default BIO)')
 parser.add_argument('--window', type=int, default=2, help = 'Window size to be considered (default 2)')
-parser.add_argument('--embeddings', type=str, default="w2v_SBWC", help = 'Embeddings to be used: w2v_SBWC, glove_SBWC, fasttext_SBWC, fasttext_SUC, fasttext_wiki, spacy (default is spacy)')
-parser.add_argument('--scaling', type=float, default=0.5, help = 'Scaling for word emebeddings (default 1.0)')
 parser.add_argument('--verbose', action='store_true', help='Prints list of false positives, true positives and false negatives (default False)')
 parser.add_argument('--stats', action='store_true', default=False, help='Print corpus numbers (number of tokens, anglicisms, headlines, etc)  (default False)')
 parser.add_argument('--include_other', action='store_true', default=True, help='Whether to include OTHER tag  (default False)')
 parser.add_argument('--collapse_tags', action='store_true', default=False, help='Whether to collapse ENGLISH and OTHER tags into a single LOANWORD tag  (default True)')
-parser.add_argument('--expanded_features', action='store_true', default=False, help = 'Include expanded features (default False)')
+parser.add_argument('--embeddings', type=str, default="w2v_SBWC", help = 'Embeddings to be used: w2v_SBWC, glove_SBWC, fasttext_SBWC, fasttext_SUC, fasttext_wiki, spacy (default is spacy)')
+parser.add_argument('--scaling', type=float, default=0.5, help = 'Scaling for word emebeddings (default 1.0)')
+
 
 
 
@@ -99,7 +91,10 @@ def custom_tokenizer(nlp):
 
 def ingest_json_document(doc_json: Mapping, nlp: Language, include_other: bool, is_predict = False) -> Doc:
     if is_predict:
-        doc = nlp(doc_json["title"] + "\n" + doc_json["text"])
+        if doc_json["annotation_approver"] == "lazaro":
+            doc = nlp(doc_json["text"])
+        else:
+            doc = nlp(doc_json["title"] + "\n" + doc_json["text"])
         doc.user_data["date"] = doc_json["date"]
         doc.user_data["url"] = doc_json["url"]
         doc.user_data["newspaper"] = doc_json["newspaper"]
@@ -164,6 +159,8 @@ def span_prf1_type_map(
     for i in range(len(reference_docs)):
         ents_from_ref = {ent for ent in reference_docs[i].ents}
         ents_from_test = {ent for ent in test_docs[i].ents}
+        print(ents_from_ref)
+        print(ents_from_test)
         if type_map is not None:
             ents_from_ref = remapping(ents_from_ref, type_map)  # ugly code, but otherwise the
             ents_from_test = remapping(ents_from_test, type_map)
@@ -311,8 +308,8 @@ def print_statistics(training, test) -> None:
 
     print(tabulate(table, headers=["", "Training", "Test"]))
 
-def train_predict(train_set, test_set, max_iterations, c1, c2, encoder, window_size) -> None:
-    features = [    WordVectorFeatureNerpy(args.embeddings, args.scaling),
+def predict(path_to_model, window_size, test_set) -> None:
+    features = [    #WordVectorFeatureNerpy(args.embeddings, args.scaling),
                     BiasFeature(),
                     TokenFeature(),
                     UppercaseFeature(),
@@ -326,81 +323,33 @@ def train_predict(train_set, test_set, max_iterations, c1, c2, encoder, window_s
                     EmailFeature(),
                     TwitterFeature()
                     ]
+                    
+    crf = CRFsuiteEntityRecognizer(WindowedTokenFeatureExtractor(features,window_size,), ENCODER_DICT[args.encoder])
+    crf.tagger = pycrfsuite.Tagger()
+    crf.tagger.open(path_to_model)
 
-    crf = CRFsuiteEntityRecognizer(WindowedTokenFeatureExtractor(features,window_size,), ENCODER_DICT[encoder])
-    if args.verbose: print("Training...")
-    crf.train(train_set, "lbfgs", {"max_iterations":  max_iterations, 'c1': c1, 'c2': c2, 'delta': args.delta}, "tmp.model")
 
     if args.verbose: print("Predicting...")
-    if args.val_folder or args.cross_validation:
-        test_set = copy.deepcopy(test_set)
-        for doc in test_set:
-            doc.ents = []
+    
+    test_set = copy.deepcopy(test_set)
+    for doc in test_set:
+        doc.ents = []
     predicted = [crf(doc) for doc in test_set]
     return predicted
-    
-def write_to_db(mydb, ent, label, context, newspaper, url, date, categoria,start, end):
-    
-    mycursor = mydb.cursor()
-    date_object = datetime.strptime(date, '%A, %d %B %Y').date()
-    date_str = date_object.strftime('%Y-%m-%d')
-    mylemma = singularize(ent) if not ent.endswith("a") and not ent.endswith(
-            "glamour") and not ent.endswith("ss") and not ent.endswith("our") else ent
-    if mylemma.endswith("ies"): # veggies, birdies
-        mylemma = ent[:-1]
-    sql = "INSERT INTO t_anglicisms (borrowing,lang,context,newspaper,url,date,section,start_token,end_token, new_date, lemma) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    val = (ent, label, context, newspaper, url, date, categoria,start, end, date_str, mylemma.lower())
-    mycursor.execute(sql, val)
 
-    mydb.commit()
     
-def connect_to_db():
-    mydb = mysql.connector.connect(host=MY_HOST,user=MY_USERNAME,password=MY_PASS,database=MY_DB)
-    return mydb
-    
-def write_predictions(predicted_docs):
-    anglicism_pd = pd.read_csv(ANGLICISM_INDEX, error_bad_lines=False)
-    try:
-        mydb = connect_to_db()
-    except Exception as e:
-        print(e)
-    with open(TO_BE_TWEETED_PATTERN + TODAY +'.csv', 'a', encoding = "utf-8", newline='') as tobetweeted, open(ANGLICISM_INDEX, 'a', encoding="utf-8", newline='') as anglicism_index:
-        anglicism_index_writer = csv.writer(anglicism_index, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        tobetweeted_writer = csv.writer(tobetweeted, delimiter=',', quoting=csv.QUOTE_MINIMAL)
-        tobetweeted_writer.writerow(["borrowing", "lang", "context", "newspaper", "url", "date", "categoria"])
-        anglicisms_of_the_day = defaultdict(int)
-        for mydoc in predicted_docs:
-            myents = list()
-            myspans = list()
-            for ent in mydoc.ents:
-                myents.append([ent.start, ent.end, ent.label_])
-                myspans.append(ent.text)
-                if ent.start < 15:
-                    context = mydoc[0:ent.end + 15].text
-                else:
-                    context = mydoc[ent.start - 15:ent.end + 15].text
-                context = context.replace("\n", ". ")
-                anglicism_index_writer.writerow([ent.text.lower(), ent.label_, context, mydoc.user_data["newspaper"], mydoc.user_data["url"], mydoc.user_data["date"], mydoc.user_data["categoria"],ent.start, ent.end])
-                try:
-                    write_to_db(mydb, ent.text, ent.label_, context, mydoc.user_data["newspaper"], mydoc.user_data["url"], mydoc.user_data["date"], mydoc.user_data["categoria"],ent.start, ent.end)
-                except Exception as e:
-                    print(e)
-                seriesObj = anglicism_pd.apply(lambda x: True if x['borrowing'] == ent.text.lower() else False, axis=1)
-                times_appeared_prev = len(seriesObj[seriesObj == True].index)
-                if times_appeared_prev == 1:
-                    context = context.replace("\'", "")
-                    context = context.replace("\"", "")
-                    context = context.replace("“", "")
-                    context = context.replace("”", "")
-                    context = context.replace("‘", "")
-                    context = context.replace("’", "")
-                    tobetweeted_writer.writerow([ent.text, ent.label_, context, mydoc.user_data["newspaper"], mydoc.user_data["url"],mydoc.user_data["date"], mydoc.user_data["categoria"]])
-                elif times_appeared_prev == 0 and anglicisms_of_the_day[ent.text.lower()] == 1:
-                    tobetweeted_writer.writerow([ent.text, ent.label_, context, mydoc.user_data["newspaper"], mydoc.user_data["url"], mydoc.user_data["date"], mydoc.user_data["categoria"]])
-                anglicisms_of_the_day[ent.text.lower()] += 1
-            mydict = {"text": mydoc.text, "date": mydoc.user_data["date"], "annotation_approver": "lazaro", "newspaper": mydoc.user_data["newspaper"], "categoria": mydoc.user_data["categoria"], "url": mydoc.user_data["url"], "labels": myents, "spans": myspans}
-            with open(AUTOMATICALLY_ANNOTATED_FOLDER + TODAY + '.jsonl', 'a', encoding = "utf-8") as f:
-                f.write(json.dumps(mydict)+'\n')
+def write_predictions(predicted_docs) -> None:
+    for mydoc in predicted_docs:
+        labels = list()
+        labels_tokens = list()
+        myspans = list()
+        for ent in mydoc.ents:
+            labels.append([mydoc[ent.start].idx, mydoc[ent.end].idx, ent.label_])
+            labels_tokens.append([ent.start, ent.end, ent.label_])
+            myspans.append(ent.text)
+        mydict = {"text": mydoc.text, "date": mydoc.user_data["date"], "annotation_approver": "lazaro", "newspaper": mydoc.user_data["newspaper"], "categoria": mydoc.user_data["categoria"], "url": mydoc.user_data["url"], "labels": labels, "labels_tokens": labels_tokens, "spans": myspans}
+        with open(args.predicted, 'a', encoding = "utf-8") as f:
+            f.write(json.dumps(mydict)+'\n')
 
 
 
@@ -411,36 +360,14 @@ if __name__ == "__main__":
     NLP.tokenizer = custom_tokenizer(NLP)
 
     if args.verbose: print(args)
-    if args.verbose: print("Loading data...")
-    training = list()
+    if args.verbose: print("Loading test data...")
+
     test = list()
-    for file in os.listdir(args.train_folder):
-        if os.path.isfile(os.path.join(args.train_folder, file)):
-            training.extend(load_data(os.path.join(args.train_folder, file), args.include_other))
-    if not args.val_folder and not args.cross_validation:# we are  in test
-        path_to_test = TO_BE_PREDICTED_FOLDER + TODAY + "/"
-        for file in os.listdir(path_to_test):
-            if os.path.isfile(os.path.join(path_to_test, file)):
-                test.extend(load_data(os.path.join(path_to_test, file), args.include_other, is_predict = True))
-        predicted_docs = train_predict(training, test, args.max_iterations, args.c1, args.c2, args.encoder, args.window)
-        write_predictions(predicted_docs)
-    elif args.val_folder: # there is validation folder
-        for file in os.listdir(args.val_folder):
-            if os.path.isfile(os.path.join(args.val_folder, file)):
-                test.extend(load_data(os.path.join(args.val_folder, file), args.include_other))
-        predicted_docs = train_predict(training, test, args.max_iterations, args.c1, args.c2, args.encoder, args.window)
-        prf1, predictions = evaluate(predicted_docs, test)
-        print(predictions)
-        print_results(prf1)
-    elif args.cross_validation: # cross validation
-        print("Cross validation mode")
-        for i in range(KFOLD): # 10 k fold
-            instances_no = len(training) // KFOLD
-            test_cv = training[instances_no*i: instances_no*i+ instances_no]
-            train_cv = training[0:instances_no*i] + training[instances_no*i+ instances_no:]
-            predicted_docs = train_predict(train_cv, test_cv, args.max_iterations, args.c1, args.c2, args.encoder, args.window)
-            prf1, predictions = evaluate(predicted_docs, test_cv)
-            print(test_cv)
-            print(predictions)
-            print_results(prf1)
-            print("####################")
+    with open(args.test, "r", encoding="utf-8") as f:
+        for line in f:
+            test.extend(load_data(line, args.include_other, is_predict=True))
+    predicted_docs = predict(args.model, args.window, test)
+    prf1, predictions = evaluate(predicted_docs, test)
+    print(predictions)
+    print_results(prf1)
+    write_predictions(predicted_docs)
