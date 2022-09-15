42023a2f764631850492dbd92989a31a822a8d61
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2022-09-15 18:10:00 +0200

    Big remove

--- bash/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate lazaro
-cat /home/ealvarezmellado/lazaro/indices/* >> /home/ealvarezmellado/lazaro/data/articles_index.csv
-rm -rfv /home/ealvarezmellado/lazaro/indices/*
-#rm -rfv /home/ealvarezmellado/lazarobot/embeddings_db/*
-python /home/ealvarezmellado/lazaro/scripts/crf.py --include_other > /home/ealvarezmellado/lazaro/logs/run.txt 2>&1 && python /home/ealvarezmellado/lazaro/scripts/tweet.py
--- bash/top.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate lazaro
-python /home/ealvarezmellado/lazaro/scripts/top.py &> /home/ealvarezmellado/error_twitter.txt
