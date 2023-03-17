332057f837043a5a1df1da47fb2fa79b169f78a8
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-03-17 10:57:48 +0100

    minor change to bash file

--- bash/observatorio.sh
+++ bash/observatorio.sh
@@ -2,7 +2,6 @@
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate observatory
-#rm -rfv /home/ealvarezmellado/lazaro/logs/*
-#rm -rfv /home/ealvarezmellado/lazaro/tobetweeted/*
-python /home/ealvarezmellado/observatory/lazaro/scripts/observatorio.py /home/ealvarezmellado/observatory/lazaro /home/ealvarezmellado/observatory/lazaro/param/param.yaml > /home/ealvarezmellado/observatory/lazaro/logs/execution.log  2>&1
-#&& python /home/ealvarezmellado/lazaro/scripts/tweet.py
+
+python /home/ealvarezmellado/observatory/lazaro/scripts/observatorio.py /home/ealvarezmellado/observatory/lazaro /home/ealvarezmellado/observatory/lazaro/param/param.yaml > /home/ealvarezmellado/observatory/lazaro/logs/execution_observatory.log  2>&1 && 
+python /home/ealvarezmellado/observatory/lazaro/scripts/tweet.py /home/ealvarezmellado/observatory/lazaro /home/ealvarezmellado/observatory/lazaro/param/param.yaml > /home/ealvarezmellado/observatory/lazaro/logs/execution_lazarobot.log 2>&1 
