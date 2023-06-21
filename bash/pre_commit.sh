9f6e9e6c0a9bce7a1d4833169c5c1dedcef590f0
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2023-06-21 11:43:03 +0200

    Improved precommit script

--- bash/pre_commit.sh
+++ bash/pre_commit.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-find . -type f -exec dos2unix {} \;
+#find . -type f -exec dos2unix {} \;
+find . -type f \( -not -path "./.git/*" -a -not -path "./.idea/*" -a -not -path "./__pycache__/*" \) -exec dos2unix {} \;
+
+#files='lazaro/ utils/ scripts/ *.py'
+
+files=(scripts/ utils)
+isort -rc "${files[@]}"
+black "${files[@]}"
+flake8 "${files[@]}"
+mypy "${files[@]}"
