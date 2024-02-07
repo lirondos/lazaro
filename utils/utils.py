a3065cb69313a6fc79ffc721610ab2cc5d0a0ab1
Elena Álvarez Mellado <ealvarezmellado@gmail.com>
2024-02-07 15:04:08 +0100

    Regex added

--- utils/utils.py
+++ utils/utils.py
@@ -63,6 +63,8 @@ def clean_html(article: Article):
     )
     article.html = re.sub(r"<blockquote cite=\".+?</blockquote>", "", article.html)
     article.html = re.sub(r"<figcaption.+?</figcaption>", "", article.html)
+    article.html = re.sub(r"<p class=\"caption-title \">.+?</p>", "", article.html)
+    article.html = re.sub(r"<span class=\"caption-author \">.+?</span>", "", article.html)
     article.html = re.sub(r"<span class=\"author .+?</span>", "", article.html)
     
     if "vertele" in article.url:
